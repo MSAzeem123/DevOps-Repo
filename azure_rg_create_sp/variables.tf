46ac17ded7469c5be2bef1d51ee17265e8f3bcb8
MSAzeem123 <mail2mohammedazeem@gmail.com>
2025-08-13 16:39:37 -0500

    commented the vairables in variable file

--- azure_rg_create_sp/variables.tf
+++ azure_rg_create_sp/variables.tf
@@ -1,22 +1,22 @@
-variable "client_id" {
-  description = "Azure Priciple client id"
-  type        = string
-}
+# variable "client_id" {
+#   description = "Azure Priciple client id"
+#   type        = string
+# }
 
-variable "client_secret" {
-  description = "The value of secret SP"
-  type        = string
-}
+# variable "client_secret" {
+#   description = "The value of secret SP"
+#   type        = string
+# }
 
-variable "tenant_id" {
-  description = "The value of SP tenant"
-  type        = string
-}
+# variable "tenant_id" {
+#   # description = "The value of SP tenant"
+#   # type        = string
+# }
 
-variable "subscription_id" {
-  description = "The value of subscription id"
-  type        = string
-}
+# variable "subscription_id" {
+#   # description = "The value of subscription id"
+#   # type        = string
+# }
 
 variable "resource_group_name" {
   description = "Name of the Azure resource group"
